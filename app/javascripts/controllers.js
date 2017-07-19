bb0de24f3119c2561e67237ecd26ace1423110d7
maicon domingues <dominguesm@outlook.com>
2017-07-19 14:31:44 -0300

    a

--- app/javascripts/controllers.js
+++ app/javascripts/controllers.js
@@ -7,13 +7,14 @@ tornadoNwController.controller('IndexCtrl', ['$scope', '$http',
 
         navigator.geolocation.getCurrentPosition(
             function (position) {
-                alert(position.coords.latitude, position.coords.longitude);
                 var lat = position.coords.latitude;
                 var lng = position.coords.longitude;
                 var url = "https://maps.googleapis.com/maps/api/geocode/json?latlng=" + lat + "," + lng + "&sensor=true";
                 $http.get(url)
                     .then(function (result) {
-                        $scope.address = result.data.results[1].formatted_address;
+                        var address = result.data.results[1].formatted_address;
+                        $scope.intro = result.data.results[1].formatted_address;
+                        alert(address);
                     });
             },
             function (error) {
