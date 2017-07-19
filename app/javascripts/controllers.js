114d7824acf21f7efe89a6c9db5f81ae14dbc4b9
maicon domingues <dominguesm@outlook.com>
2017-07-19 14:25:03 -0300

    a

--- app/javascripts/controllers.js
+++ app/javascripts/controllers.js
@@ -5,33 +5,27 @@ var tornadoNwController = angular.module('tornadoNwController', []);
 tornadoNwController.controller('IndexCtrl', ['$scope', '$http',
     function ($scope, $http) {
 
-        if (navigator.geolocation) navigator.geolocation.getCurrentPosition(
-            onPositionUpdate, error, {
+        navigator.geolocation.getCurrentPosition(
+            function (position) {
+                alert(position.coords.latitude, position.coords.longitude);
+                var lat = position.coords.latitude;
+                var lng = position.coords.longitude;
+                var url = "https://maps.googleapis.com/maps/api/geocode/json?latlng=" + lat + "," + lng + "&sensor=true";
+                $http.get(url)
+                    .then(function (result) {
+                        var address = result.data.results[2].formatted_address;
+                        $scope.address = address;
+                        $scope.intro = result.data.results[1].formatted_address;
+                        alert(address);
+                    });
+            },
+            function (error) {
+                alert(error.message);
+            }, {
                 enableHighAccuracy: true
-                , timeout: 2000
+                , timeout: 5000
             }
         );
-
-        function onPositionUpdate(position) {
-            var lat = position.coords.latitude;
-            var lng = position.coords.longitude;
-            var url = "https://maps.googleapis.com/maps/api/geocode/json?latlng=" + lat + "," + lng + "&sensor=true";
-            $http.get(url)
-                .then(function (result) {
-                    var address = result.data.results[2].formatted_address;
-                    $scope.address = address;
-                    $scope.intro = result.data.results[1].formatted_address;
-                    alert(address);
-                });
-        };
-
-        function error(err) {
-            if (err.code == 1) {
-                alert("Error: Access is denied!");
-            } else if (err.code == 2) {
-                alert("Error: Position is unavailable!");
-            }
-        };
 
     }
 ]);
